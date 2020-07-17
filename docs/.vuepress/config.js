818e399cb9e22b7a1953c7f5185ccbedd9078b61
Hedzr Yeh <hedzrz@gmail.com>
2020-07-17 16:02:12 +0800

    update icons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,14 +15,17 @@ module.exports = ctx => ({
     }
   },
   head: [
+    // ['link', { rel: 'shortcut icon', type: "image/x-icon", href: `./favicon.ico` }],
+    ['link', { rel: 'shortcut icon', href: `./logo.png` }],
     ['link', { rel: 'icon', href: `/logo.png` }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon.png` }],
+    // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    // ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/android-chrome-192x192.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   theme: 'carbon',
